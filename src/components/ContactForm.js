3629ef5a2b48201be78b99a4f25c6c74229f69d2
Josiah Roa <josiahroa18@gmail.com>
2020-03-12 16:37:54 -0600

    Finished MVP

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,24 +16,26 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id='firstName'
             name="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 10 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid='error'>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id='lastName'
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p data-testid='error'>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
@@ -41,21 +43,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id='email' name="email" ref={register({ required: true })} />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p data-testid='error'>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id='message' name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid='output' style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type="submit" id='submit'/>
       </form>
     </div>
   );
---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,167 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from "./ContactForm";
+
+test('renders correctly', () => {
+    render(<ContactForm/>)
+})
+
+test('contact form adds new contact', async () => {
+    const { findByLabelText, findByTestId} = render(<ContactForm/>);
+
+    // Check to see if labels and inputs exist
+    const firstNameInput = await findByLabelText(/first name*/i);
+    const lastNameInput = await findByLabelText(/last name*/i);
+    const emailInput = await findByLabelText(/email*/i);
+    const messageInput = await findByLabelText(/message/i);
+
+    // Fill out all input fields
+    fireEvent.change(firstNameInput, {
+        target: {name: 'firstName', value: 'Josiah'}
+    });
+
+    fireEvent.change(lastNameInput, {
+        target: {name: 'lastName', value: 'Roa'}
+    });
+
+    fireEvent.change(emailInput, {
+        target: {name: 'email', value: 'josiahroa18@gmail.com'}
+    });
+
+    fireEvent.change(messageInput, {
+        target: {name: 'message', value: 'Hello, this is a message'}
+    });
+
+    // Check to see if submit button works
+    fireEvent.click(document.getElementById('submit'));
+
+    // Check to see if the output appears
+    await findByTestId('output');
+})
+
+// Test to see if lack of first name toggles first name error
+test('first name error appears', async () => {
+    const { findByLabelText, findByTestId } = render(<ContactForm/>)
+
+    // Check to see if labels and inputs exist
+    const firstNameInput = await findByLabelText(/first name*/i);
+    const lastNameInput = await findByLabelText(/last name*/i);
+    const emailInput = await findByLabelText(/email*/i);
+    const messageInput = await findByLabelText(/message/i);
+
+    // Since we are testing if an error appears if there is no name, we do not add a name
+    // fireEvent.change(firstNameInput, {
+    //     target: {name: 'firstName', value: 'Josiah'}
+    // });
+
+    fireEvent.change(lastNameInput, {
+        target: {name: 'lastName', value: 'Roa'}
+    });
+
+    fireEvent.change(emailInput, {
+        target: {name: 'email', value: 'josiahroa18@gmail.com'}
+    });
+
+    fireEvent.change(messageInput, {
+        target: {name: 'message', value: 'Hello, this is a message'}
+    });
+
+    fireEvent.click(document.getElementById('submit'));
+
+    await findByTestId('error');
+})
+
+// Test to see if an error appears if the first name exceeds max length of 10
+test ('first name is too long', async() => {
+    const { findByLabelText, findByTestId } = render(<ContactForm/>)
+
+    // Check to see if labels and inputs exist
+    const firstNameInput = await findByLabelText(/first name*/i);
+    const lastNameInput = await findByLabelText(/last name*/i);
+    const emailInput = await findByLabelText(/email*/i);
+    const messageInput = await findByLabelText(/message/i);
+
+    fireEvent.change(firstNameInput, {
+        target: {name: 'firstName', value: 'Josiahasdfas'}
+    });
+
+    fireEvent.change(lastNameInput, {
+        target: {name: 'lastName', value: 'Roa'}
+    });
+
+    fireEvent.change(emailInput, {
+        target: {name: 'email', value: 'josiahroa18@gmail.com'}
+    });
+
+    fireEvent.change(messageInput, {
+        target: {name: 'message', value: 'Hello, this is a message'}
+    });
+
+    fireEvent.click(document.getElementById('submit'));
+
+    await findByTestId('error');
+})
+
+// Test to see if lack of last name toggles last name error
+test('last name error appears', async () => {
+    const { findByLabelText, findByTestId } = render(<ContactForm/>)
+
+    // Check to see if labels and inputs exist
+    const firstNameInput = await findByLabelText(/first name*/i);
+    const lastNameInput = await findByLabelText(/last name*/i);
+    const emailInput = await findByLabelText(/email*/i);
+    const messageInput = await findByLabelText(/message/i);
+
+    fireEvent.change(firstNameInput, {
+        target: {name: 'firstName', value: 'Josiah'}
+    });
+
+    // Since we are testing if an error appears if there is no name, we do not add a name
+    // fireEvent.change(lastNameInput, {
+    //     target: {name: 'lastName', value: 'Roa'}
+    // });
+
+    fireEvent.change(emailInput, {
+        target: {name: 'email', value: 'josiahroa18@gmail.com'}
+    });
+
+    fireEvent.change(messageInput, {
+        target: {name: 'message', value: 'Hello, this is a message'}
+    });
+
+    fireEvent.click(document.getElementById('submit'));
+
+    await findByTestId('error');
+})
+
+// Test to see if lack of input toggles email error
+test('email error appears', async() => {
+    const { findByLabelText, findByTestId } = render(<ContactForm/>)
+
+    // Check to see if labels and inputs exist
+    const firstNameInput = await findByLabelText(/first name*/i);
+    const lastNameInput = await findByLabelText(/last name*/i);
+    const emailInput = await findByLabelText(/email*/i);
+    const messageInput = await findByLabelText(/message/i);
+
+    fireEvent.change(firstNameInput, {
+        target: {name: 'firstName', value: 'Josiah'}
+    });
+
+    fireEvent.change(lastNameInput, {
+        target: {name: 'lastName', value: 'Roa'}
+    });
+
+    // Since we are testing if an error appears if there is no name, we do not add a name
+    // fireEvent.change(emailInput, {
+    //     target: {name: 'email', value: 'josiahroa18@gmail.com'}
+    // });
+
+    fireEvent.change(messageInput, {
+        target: {name: 'message', value: 'Hello, this is a message'}
+    });
+
+    fireEvent.click(document.getElementById('submit'));
+
+    await findByTestId('error');
+})
